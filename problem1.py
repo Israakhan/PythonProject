56c7b66735a6409a82df5a623d6656006bc22693
Isra khan <kisra4414@gmail.com>
2020-12-29 00:02:18 -0800

    pythod code file added

--- problem1.py
+++ problem1.py
@@ -1,1 +1,8 @@
-print ("Hello World")
+sum = 0
+for i in range(1000000):
+    bina = int(str(bin(i)).replace('0b',''))
+    if(i==int(str(i)[::-1]))or(bina==int(str(bina)[::-1])):
+        #print("i : "+str(i))
+        #print("bina : "+str(bina))
+        sum+=i
+print("Sum of numbers : ",sum)
